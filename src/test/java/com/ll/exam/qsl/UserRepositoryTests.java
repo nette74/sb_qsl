dcbd3c47e56db21ef19385b59e0a94e6c17dc726
jhs512 <jangka512@gmail.com>
2022-08-26 14:27:27 +0900

    getQslUser 테스트케이스 추가

--- src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
+++ src/test/java/com/ll/exam/qsl/UserRepositoryTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class UserRepositoryTests {
     @Autowired
@@ -21,5 +23,16 @@ class UserRepositoryTests {
         SiteUser u2 = new SiteUser(null, "user2", "{noop}1234", "user2@test.com");
 
         userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+
+    @Test
+    @DisplayName("1번 회원을 Qsl로 가져오기")
+    void t2() {
+        SiteUser u1 = userRepository.getQslUser(1L);
+
+        assertThat(u1.getId()).isEqualTo(1L);
+        assertThat(u1.getUsername()).isEqualTo("user1");
+        assertThat(u1.getEmail()).isEqualTo("user1@test.com");
+        assertThat(u1.getPassword()).isEqualTo("{noop}1234");
     }
 }
